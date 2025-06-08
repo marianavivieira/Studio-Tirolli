1d108ead75546e05680f09c5e3d650c2535d20b5
marianavivieira <mariana.carvalhov@outlook.com>
2025-06-07 21:10:33 -0300

    teste
    teste

--- js/index.js
+++ js/index.js
@@ -36,3 +36,12 @@ function criarIndicadores() {
 }
 
 window.onload = criarIndicadores;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
+  const navList = document.getElementById('nav-list');
+
+  toggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
+});
